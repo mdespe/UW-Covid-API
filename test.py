8c378f9a4d0c8fc83d950cc42b0a526fb88ea175
mdespe <mikespe23@gmail.com>
2020-09-09 13:11:58 -0500

    testing

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.get('https://infinite-shore-86765.herokuapp.com/uw-covid')
-print(response.text)
+response = requests.get('https://infinite-shore-86765.herokuapp.com/uw-covid/Sep 7')
+print(response.json())
